3806c8bfb50ccb91e6ce8c5961ed913e161499e0
Tanguy-Supinfo <tanguy.joseph@supinfo.com>
2022-02-23 15:35:14 +0100

    add auth midleware

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 // const rateLimit = require("express-rate-limit");
+const firebase = require("./utils/firebase.js");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
@@ -27,10 +28,46 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Apply the limit to all requests
 // app.use(limiter);
 
+// Check if the endpoint should be authenticated
+app.use((req, res, next) => {
+  switch (req.path) {
+    case "/v1/auth/login":
+    case "/v1/auth/register":
+    case "/v1/auth/recovery":
+    case "/v1/auth/refresh":
+      req.bypass = true;
+      break;
+    default:
+      req.bypass = false;
+      break;
+  }
+
+  next();
+});
+
 // Check the validity of the token
-/* app.use(function(req, res, next) {
-  return next;
-});*/
+app.use((req, res, next) => {
+  if (req.bypass) return next();
+  if (!req.headers.authorization) {
+    req.headers.authorization = "null";
+  }
+
+  const reqAuth = req.headers.authorization
+      .replace("Bearer ", "");
+
+  firebase.auth.verifyIdToken(reqAuth)
+      .then((decodedToken) => {
+        req.userId = decodedToken.uid;
+        next();
+      })
+      .catch(() => {
+        res.status(400).json({
+          "status": "error",
+          "code": 0,
+          "detail": "Invalid or expired token !",
+        });
+      });
+});
 
 // All route by slice
 require("./routes/auth.routes.js")(app);
