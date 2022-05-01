21e8c06b3f5ba2269ad57a68108e875d8e258c82
Tanguy-Supinfo <tanguy.joseph@supinfo.com>
2022-05-01 18:46:43 +0200

    Upload system V2 & get data update

--- functions/controllers/bucket/data.controller.js
+++ functions/controllers/bucket/data.controller.js
@@ -1,10 +1,11 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable max-len */
 const firebase = require("../../utils/firebase.js");
 
 // ** DATA **
 exports.endpoint = (req, res) => {
   if (req.query.path === undefined || req.query.path.length < 1) {
-    res.status(400).json({
+    return res.status(400).json({
       "status": "error",
       "code": 12,
       "detail": "INVALID_QUERY",
@@ -14,10 +15,21 @@ exports.endpoint = (req, res) => {
   const formatFiles = [];
 
   const folderPrefix = req.userId + req.query.path;
-  firebase.bucket.getFiles({prefix: folderPrefix}, function(err, files) {
+  firebase.bucket.getFiles({
+    prefix: folderPrefix,
+    delimiter: "/",
+    includeTrailingDelimiter: true,
+  }, function(err, files) {
     if (!err) {
       files.forEach((file) => {
-        formatFiles.push(file.name.replace(folderPrefix, ""));
+        console.log(file);
+        formatFiles.push({
+          name: (file.name.slice(-1) != "/") ? file.name.replace(folderPrefix, "") : file.name.replace(folderPrefix, "").slice(0, -1),
+          type: (file.name.slice(-1) != "/") ? file.metadata.contentType : "folder",
+          created: file.metadata.timeCreated,
+          updated: file.metadata.updated,
+          size: file.metadata.size,
+        });
       });
       formatFiles.shift();
       console.log(formatFiles);
--- functions/controllers/bucket/upload.controller.js
+++ functions/controllers/bucket/upload.controller.js
@@ -7,6 +7,15 @@ exports.endpoint = (req, res) => {
   const form = new formidable.IncomingForm();
   return new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
+      if (fields.path === undefined || fields.path < 1) {
+        reject(new Error("Invalid PATH !"));
+        return res.status(400).json({
+          "status": "error",
+          "code": 12,
+          "detail": "INVALID_QUERY",
+        });
+      }
+
       const file = files.file;
       if (!file) {
         reject(new Error("no file to upload, please choose a file."));
@@ -15,7 +24,7 @@ exports.endpoint = (req, res) => {
 
       const response = firebase.bucket.upload(file.path, {
         contentType: file.type,
-        destination: req.userId + "/" + file.name,
+        destination: req.userId + fields.path + file.name,
       });
 
       resolve({
