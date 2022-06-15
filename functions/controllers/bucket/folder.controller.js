c75c2e350a09700eaa4423ac932f143de7f5fda9
Tanguy-Supinfo <tanguy.joseph@supinfo.com>
2022-06-15 19:27:03 +0200

    create folder system

---
+++ functions/controllers/bucket/folder.controller.js
@@ -0,0 +1,45 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+const firebase = require("../../utils/firebase.js");
+
+// ** FOLDER **
+exports.endpoint = (req, res) => {
+  // check if all body parameters are correct
+  if (!checkBodyParams(req.body)) {
+    return res.status(400).json({
+      "status": "error",
+      "code": 2,
+      "detail": "INCORRECT_PARAMETERS",
+    });
+  }
+
+  const file = firebase.bucket.file("newfolder/");
+  file.copy(req.userId + req.body.path + req.body.name + "/", function(err, rep, copy) {
+    if (err) {
+      return res.status(500).json({
+        "status": "error",
+        "code": 20,
+        "detail": "UNABLE_CREATE_FOLDER",
+      });
+    } else {
+      return res.status(200).json({
+        "status": "success",
+        "detail": "The folder has been created !",
+      });
+    }
+  });
+};
+
+/**
+ * Check the validity of the query parameters
+ * @param {Array} bodyParam List of query parameters
+ * @return {boolean} Returns true if all parameters are correct
+ */
+function checkBodyParams(bodyParam) {
+  // Check for path [required]
+  if (!bodyParam.path) return false;
+  // Check for name [required]
+  if (!bodyParam.name) return false;
+
+  return true;
+}
